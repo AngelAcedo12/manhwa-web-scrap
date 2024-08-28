3249f66df6c5b24a5a6c940d047ddb789a1ded44
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-08-28 20:40:53 +0200

    feat: Add functionality to download manga chapters
    This commit adds functionality to download manga chapters from a given URL. It prompts the user for the path to save the manga, the URL of the manga, and the maximum number of chapters to download. It then creates a directory for the manga, downloads each chapter's images, and saves them in the corresponding chapter directory.
    
    The code also includes error handling for various scenarios, such as failed directory creation, failed image downloads, and general exceptions. It keeps track of the number of successfully downloaded chapters and displays the progress as a percentage.
    
    This commit addresses the need to download manga chapters and improves the user experience by providing feedback on the progress of the download.

--- scrapedManga.py
+++ scrapedManga.py
@@ -9,32 +9,37 @@ dir_saved = str(input("Enter the path to the save Manga: "))
 urlPri = str(input("Enter the path to the file: "))
 maxCap = int(input("Enter the number of cap: ")) 
 name = str(input("Enter the name of the manga: "))
+capComplete = 0
 def download(urlPri, maxCap):
+    errors_in_download = 0
     dir_path = path.join(dir_saved,name)
     try:
         mkdir(dir_path)
     except Exception as e:  
         print(f"Error: {e}")   
-        # 
+         
         
     for i in range(1, maxCap+1):
-
+            
             url = urlPri+'-'+str(i)
+            print('URL: '+ url)
             file_name = PurePath(url).name
             print(f"Downloading {file_name}...")
             try: 
                 mkdir(path.join(dir_path, file_name),dir_fd=None)
             except Exception as e:
                 print(f"Error: {e}")
-            finally:
-                try:
+           
+            try:
                     responseCap = get(url)
+                    
                     if responseCap.ok:
                         print('--Content find--- \n') 
                         content= responseCap.text
                         jsonContent = parseJson(content)    
                         cap = jsonToObj(jsonContent)
                         print(f"Cap {cap.chapter} downloaded")
+
                         for img in cap.imgUrls:
                             imgName = PurePath(img).name
                             imgResponse = get(img)
@@ -44,10 +49,15 @@ def download(urlPri, maxCap):
                                     file.write(imgResponse.content)
                                                     
                             else:
-                                print(f"Error downloading image {imgName}")
-                except Exception as e:
-                    print(f"Error: {e}")
-                    continue                           
+                                print(f"Error downloading image {imgName} ❌")
+                    capComplete += 1
+                    print(f"Cap {cap.chapter} downloaded \n ✅" )
+                    print(f"Total caps downloaded: {capComplete} \n " + "Progress: " + str((capComplete/maxCap)*100) + "%") 
+                    
+            except Exception as e:
+                print(f"Error: {e}")
+                errors_in_download += 1
+                continue                           
                        
                    
                         
@@ -62,9 +72,8 @@ def parseJson(content):
 def jsonToObj(jsonContent):
     return cap(jsonContent['name'], jsonContent['chapter']['chapter'],jsonContent['chapter']['img'], jsonContent['_id'])
 
-
-
 if __name__ == '__main__':
     download(urlPri, maxCap)
-    print('Download complete')
+
+    print(' --- Download complete --- ✅')
     
